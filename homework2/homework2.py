0ef610a24c00324a8d6b06c114068258b12c215b
Mykyta <nickpolshcha@gmail.com>
2024-01-18 19:02:25 +0200

    second version of homework.py added that uses divmod

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,5 +1,17 @@
+# user_input = input().replace(" ", "")
+#
+# if len(user_input) == 4 and user_input.isdigit():
+#     for i in user_input:
+#         print(i, end="\n")
+
+NUMBER_OF_DIGITS = 4
+
 user_input = input().replace(" ", "")
 
-if len(user_input) == 4 and user_input.isdigit():
-    for i in user_input:
-        print(i, end="\n")
+if user_input.isdigit():
+    quotient, remainder = divmod(len(user_input), NUMBER_OF_DIGITS)
+
+    if quotient == 1 and remainder == 0:
+        for i in user_input:
+            print(i, end="\n")
+
