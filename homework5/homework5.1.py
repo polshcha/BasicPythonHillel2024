efb23b07460e4c18b32a5b4fe76f423e99db27ed
Mykyta <nickpolshcha@gmail.com>
2024-01-28 00:32:43 -0800

    homework 5.1 vrsion 2.0 added

---
+++ homework5/homework5.1.py
@@ -0,0 +1,44 @@
+import keyword
+
+
+def if_variable_ok(var):
+    kwlist = keyword.kwlist  # ключові слова
+    punc = "!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~"  # змінена пунктуація не включаємо _ та робимо єкранування
+
+    if var not in kwlist:
+        is_keyword = False
+    else:
+        is_keyword = True
+
+    is_punc = False
+# двійний цикл перевіряємо кожний символ var чи є він пунктуацією, без цього шукає тільк. на початку (?):
+    for i in range(len(var)):
+        for j in punc:
+            if var[i] in punc:
+                is_punc = True
+                break
+        if is_punc is True:
+            break
+
+    if var[0].isdigit() or " " in var.strip():  # чи 1 симв. змінної число та чи пробіли (крім поч. та кін)
+        is_first_digit_or_space = True
+    else:
+        is_first_digit_or_space = False
+
+    if var == "_":  # тому що _ при перевірці на регістр повертає .islower FALSE оброблюємо окремо
+        is_upper = False
+    elif var != "_" and var.islower():  # якщо var НЕ Є _ і при цьому нижній регістр
+        is_upper = False
+    else:
+        is_upper = True
+
+    if is_keyword or is_punc or is_first_digit_or_space or is_upper:  # якщо хоч десь тригернуло True
+        var_ok = False  # змінна не ок
+    else:  # все норм по всім пунктам
+        var_ok = True  # змінна ОК
+
+    return var_ok
+
+
+users_var = input("~ input variable name: ")
+print(if_variable_ok(users_var))
